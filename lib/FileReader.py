6f96c220657e64a6625472c3a323597b23417266
Anton Vasilev <bysslaev@gmail.com>
2018-04-07 01:11:47 +0300

    created filereader

--- lib/FileReader.py
+++ lib/FileReader.py
@@ -5,3 +5,21 @@
  Time: 01:06
 
 """
+from lib.FileHelper import FileHelper
+
+
+class FileReader(FileHelper):
+    """
+    Класс для чтения из текстового файла
+    """
+
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.type = self.TYPE_READ
+
+    def readLine(self):
+        """
+        Читает строку из файла
+        :return:
+        """
+        return self.file.readline()
