b544b0beaf60281ae67251298679bf2c54a3a800
Anton Vasilev <bysslaev@gmail.com>
2018-04-07 01:03:29 +0300

    created file writer

--- lib/FileHelper.py
+++ lib/FileHelper.py
@@ -5,3 +5,36 @@
  Time: 00:47
 
 """
+
+import os
+
+
+class FileHelper:
+    """
+    Класс для работы с файлами
+    """
+
+    TYPE_READ = 'r'
+    TYPE_WRITE = 'w'
+
+    def __init__(self, filename):
+        self.file = None
+        self.filename = filename
+        self.type = self.TYPE_READ
+
+    def open(self):
+        """
+        Метод для открытия файла
+        :param type:
+        :param filename:
+        :return:
+        """
+        if os.path.isfile(self.filename):
+            self.file = open(self.filename, self.type)
+
+    def close(self):
+        """
+        Закрываем файл
+        :return:
+        """
+        self.file.close()
--- lib/FileWriter.py
+++ lib/FileWriter.py
@@ -5,3 +5,23 @@
  Time: 00:46
 
 """
+from lib.FileHelper import FileHelper
+
+
+class FileWriter(FileHelper):
+    """
+    Класс для записи данных в текстовый файл
+    """
+
+    def __init__(self, filename):
+        super().__init__(filename)
+        self.type = self.TYPE_WRITE
+
+    def writeLine(self, text):
+        """
+        Записывает строку в файл
+        :param text:
+        :return:
+        """
+        self.file.write(text)
+        self.file.write('\n')
